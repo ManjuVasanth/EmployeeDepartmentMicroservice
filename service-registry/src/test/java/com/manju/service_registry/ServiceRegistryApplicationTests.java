f2fa2d880228fbb48d1f4e9f9d7f25aaa8cc8bd3
Manju Vasanth <74921345+ManjuVasanth@users.noreply.github.com>
2025-02-20 21:09:07 -0600

    communication via API gateway

---
+++ service-registry/src/test/java/com/manju/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manju.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
